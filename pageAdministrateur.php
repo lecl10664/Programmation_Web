2174c8304f3a9cf82aace57a0ffe0ae4a0778887
Milan <milanserfaty@hotmail.fr>
2020-03-02 16:26:22 +0100

    Ajout de la page Administrateur

---
+++ pageAdministrateur.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Page</title>
+    <link rel="stylesheet" href="page.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <?php include "./header.html" ?>
+    <div id="content">
+
+        <div id="profil">
+            <h3 class="profil-titre">MON PROFIL</h3>
+
+            <div class="profil-colonnes">
+                <div class="profil-texte">
+                    <p>Nom Prénom</p>
+                    <p>Age</p>
+                    <p>Date de naissance</p>
+                    <p>Adresse</p>
+                </div>
+                <img class="profil-photo" src="profil_400x400.png" title="Infinite Measures"></img>
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+
+</body>
+</html>
