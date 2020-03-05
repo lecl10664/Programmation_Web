992452f3f5cbe9a655524ae38d5eda1ec87a9006
Thomas Durgetto <thomas.durgetto@numericable.fr>
2020-03-05 11:52:38 +0100

    Create editerProfil.php

---
+++ editerProfil.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Editer son profil</title>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="bodyAccueil.css">
+    <head>
+
+    <header>
+            <?php include "./header/header.html" ?>
+    </header>
+
+    <body>
+        <div class="website">
+            <main id="main" role="main" class="main">
+            <p>test</p>
+            </main>
+        </div>
+    </body>
+
+    <footer>
+        <?php include "./footer/footer.html" ?>
+    </footer>
+
+
+
+
