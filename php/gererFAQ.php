d3682a20963a1336165c3450e9f7bb4155f84383
durgettoto <thomas.durgetto@numericable.fr>
2020-04-18 20:25:47 +0200

    FAQ pour le gestionnaire + maj BDD

--- php/gererFAQ.php
+++ php/gererFAQ.php
@@ -2,48 +2,43 @@
     $dir2 = substr($_SERVER['SCRIPT_FILENAME'], 0, -strlen($_SERVER['SCRIPT_NAME']));
     chdir($dir2.DIRECTORY_SEPARATOR);
     //echo getcwd()."<br>";
-
     $bdd = new PDO('mysql:host=localhost;dbname=appg9b;charset=utf8', 'root', '');
 
     //$req = $bdd->prepare('INSERT INTO faq (N°_FAQ, Contenu) VALUES(:N°_FAQ, :Contenu)');
     $questions = $bdd->query('SELECT * FROM faq');
 ?>
+
+
 <!DOCTYPE html>
 <html>
     <head>
-        <title>FAQ</title>
+        <title>Gérer la FAQ</title>
         <?php include "./php/header.php" ?>
         <meta charset="utf-8" />
         <link rel="stylesheet" href="/css/faq.css">
-    <head>
+    </head>
     <body>
         <h1>
             FAQ
         </h1>
         <div id = content>
-            <ol>
-              <?php
+        <?php
     while ($questionsDonnees = $questions->fetch())
 {
 ?>
+    <ol>
     <li>
-        <?php echo $questionsDonnees['Questions']; ?>
-    </li>
-    <p>
-        <?php echo $questionsDonnees['Réponses']; ?>
-     </p>
+        <input style="border:none; border-bottom: 1px solid; border-bottom-color: gray;" type="text" size="100" value="<?php echo $questionsDonnees['Questions']; ?>">
+        <input type="button" value = "Ajouter ou modifier une question">
+        <input style="border:none; border-bottom: 1px solid; border-bottom-color: gray;" type="text" size="100" value="<?php echo $questionsDonnees['Réponses']; ?>">
+        <input type="button" value = "Ajouter ou modifier une réponse">
+     </li>
+    </ol>
 <?php
 }
 $questions->closeCursor();
 ?>
-          </li>
-          <p>
-           </p>
-      </ol>
         </div>
-      <h2>
-        Vous avez toujours une question ? <a href="/php/contact.php" target="_blank">Nous contacter</a> 
-      </h2>
     </body>
     <footer>
         <?php include "./php/footer.php" ?>
