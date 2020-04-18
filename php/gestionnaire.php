d026348f720395b04c8b08d9cfd2c83dbd7ed127
janiehnrd <58337469+janiehnrd@users.noreply.github.com>
2020-04-18 18:14:45 +0200

    modif janie

--- php/gestionnaire.php
+++ php/gestionnaire.php
@@ -43,8 +43,8 @@ chdir($dir2.DIRECTORY_SEPARATOR);
 
 </div>
 
-<div id="choix">
-    <p> Choisir :</p>
+<div class="choix">
+    <p> Quels test voulez-vous lancer ? </p>
     <div id="boutons">
         <div id="tout">
             <button class="button"> Tous les tests </button>
@@ -69,10 +69,22 @@ chdir($dir2.DIRECTORY_SEPARATOR);
             <button class="button">Fréq cardiaque <br/> après-test</button>
         </div>
     </div>
+    <button class="button_valider">Valider</button>
 </div>
 
+<script
+        src="https://code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+        crossorigin="anonymous">
+</script>
 
 <script>
+
+    $('.button_play').click(function(e) {
+        e.preventDefault();
+        $('.choix').toggleClass('active');
+    })
+
     var listeBouton = document.getElementsByClassName("button");
     for (var i=0; i<listeBouton.length; i++) {
         var element = listeBouton[i];
@@ -88,6 +100,67 @@ chdir($dir2.DIRECTORY_SEPARATOR);
                 element.style.color="white";}
         };
     }
+
+    function rebour1(tps)
+    {
+        if (tps>0)
+        {
+            var heure = Math.floor(tps/3600);
+            if(heure >= 24)
+            {
+                var jour = Math.floor(heure/24);
+                var moins = 86400*jour;
+                var heure = heure-(24*jour);
+            }
+            else
+            {
+                var jour = 0;
+                var moins = 0;
+            }
+            moins = moins+3600*heure;
+            var minutes = Math.floor((tps-moins)/60);
+            moins = moins + 60*minutes;
+            var secondes = tps-moins;
+            minutes = ((minutes < 10) ? "0" : "") + minutes;
+            secondes = ((secondes < 10) ? "0" : "") + secondes;
+            //document.getElementById("compteRebour_affiche").innerHTML = 'Temps restant avant lancement de la partie : '+secondes;
+            var restant = tps-1;
+            setTimeout("rebour("+restant+")", 1000);
+        }
+        else
+        {
+            //document.getElementById("compteRebour_affiche").innerHTML = 'chargement ...';
+        }
+    }
+
+    function rebour2(tps){ //Deffinition d'une fonction
+        if (tps>0) { //Si le temps est différent de 0
+            var heure = Math.floor(tps/3600); //Nombre d'heure écoulés
+            if(heure >= 24){ //Si plus de 24 => 1 jour
+                var jour = Math.floor(heure/24); //Calcul du nombre de jour
+                var moins = 86400*jour; // Deffinition et attribution d'une valeur à `moins` qui est la variable soustractrice de la fonction
+                var heure = heure-(24*jour); //On enléve le nombre d'heure concernée
+            }else{
+                var jour = 0; //Sinon, il n'y a pas de jour
+                var moins = 0; // Et pas ed variable moins
+            }
+            moins = moins+3600*heure; // Recalcul
+            var minutes = Math.floor((tps-moins)/60); // Calcul des minutes
+            moins = moins + 60*minutes; // Recalcul de la variable moins
+            var secondes = tps-moins; //Calcul des seconde
+            minutes = ((minutes < 10) ? "0" : "") + minutes;//On rajoute un 0 si les minutes sont inférieures à 10
+            secondes = ((secondes < 10) ? "0" : "") + secondes; //On rajoute un 0 si les secondes sont inférieures à 10
+            //document.getElementById("compteRebour_affiche").innerHTML = 'Temps restant : '+jour+' Jours, '+heure+':'+minutes+':'+secondes; //On affiche le resultat dans le div concerné
+            var restant = tps-1; //On enléve une seconde
+            setTimeout("rebour("+restant+")", 1000);//On rappelle la fonction toute les secondes
+        }else{
+            alert("Compte à rebour fini !"); //Message
+        }
+    }
+
+    document.getElementsByClassName("button_valider").onclick=rebour2(3);
+    $( ".button_valider" ).click(rebour2(3));
+
 </script>
 
 </body>
