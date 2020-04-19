f386b25b1216be4eba17f56feff581d50bc5ba5c
lecl10664 <leopold.clement@isep.fr>
2020-04-19 16:18:56 +0200

    import bdd pour regler le probleme de léo

--- php/gererFAQ.php
+++ php/gererFAQ.php
@@ -2,8 +2,12 @@
     $dir2 = substr($_SERVER['SCRIPT_FILENAME'], 0, -strlen($_SERVER['SCRIPT_NAME']));
     chdir($dir2.DIRECTORY_SEPARATOR);
     //echo getcwd()."<br>";
-    $bdd = new PDO('mysql:host=localhost;dbname=appg9b;charset=utf8', 'root', '');
-
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=appg9b;port=3308;charset=utf8', 'root', '');
+}
+catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
     //$req = $bdd->prepare('INSERT INTO faq (N°_FAQ, Contenu) VALUES(:N°_FAQ, :Contenu)');
     $questions = $bdd->query('SELECT * FROM faq');
 ?>
