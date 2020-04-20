89ddee24ea20572e663c69becd62e69bb2c017c8
lecl10664 <leopold.clement@isep.fr>
2020-04-20 12:59:02 +0200

    Update appg9b.sql

--- base_de_données/appg9b.sql
+++ base_de_données/appg9b.sql
@@ -2,15 +2,9 @@
 -- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
-<<<<<<< HEAD
--- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 20 avr. 2020 à 09:40
--- Version du serveur :  10.4.10-MariaDB
-=======
 -- Hôte : 127.0.0.1:3308
--- Généré le :  lun. 20 avr. 2020 à 09:19
+-- Généré le :  lun. 20 avr. 2020 à 10:51
 -- Version du serveur :  8.0.18
->>>>>>> leopold
 -- Version de PHP :  7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -36,22 +30,10 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `administrateur`;
 CREATE TABLE IF NOT EXISTS `administrateur` (
-<<<<<<< HEAD
-<<<<<<< HEAD
   `ID_Administrateur` int(4) NOT NULL,
-<<<<<<< HEAD
-  `Mot_de_passe` varchar(100) NOT NULL,
-=======
-=======
-  `ID_Administrateur` int(4) NOT NULL AUTO_INCREMENT,
->>>>>>> parent of ba6e8bc... Update appg9b.sql
-=======
-  `ID_Administrateur` int(4) NOT NULL AUTO_INCREMENT,
->>>>>>> parent of ba6e8bc... Update appg9b.sql
   `Mot_de_passe` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
->>>>>>> leopold
   PRIMARY KEY (`ID_Administrateur`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -62,33 +44,22 @@ CREATE TABLE IF NOT EXISTS `administrateur` (
 DROP TABLE IF EXISTS `faq`;
 CREATE TABLE IF NOT EXISTS `faq` (
   `N°_FAQ` int(11) NOT NULL,
-  `Contenu` text NOT NULL,
   `Questions` varchar(100) NOT NULL,
   `Réponses` varchar(300) NOT NULL,
-  `ID_Aministrateur` int(4) NOT NULL,
   PRIMARY KEY (`N°_FAQ`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `faq`
 --
 
-INSERT INTO `faq` (`N°_FAQ`, `Contenu`, `Questions`, `Réponses`, `ID_Aministrateur`) VALUES
-(1, '', '', '', 0),
-(2, '', '', '', 0),
-(3, '', '', '', 0),
-(4, '', '', '', 0),
-(5, '', '', '', 0),
-(6, '', '', '', 0),
-(7, '', 'Array', 'Array', 0),
-(8, '', 'Array', 'Array', 0),
-(9, '', 'Array', 'Array', 0),
-(10, '', 'Array', 'Array', 0),
-(11, '', 'Array', 'Array', 0),
-(12, '', 'Array', 'Array', 0),
-(13, '', 'Array', 'Array', 0),
-(14, '', 'Array', 'Array', 0),
-(15, '', 'Array', 'Array', 0);
+INSERT INTO `faq` (`N°_FAQ`, `Questions`, `Réponses`) VALUES
+(1, '', ''),
+(2, '', ''),
+(3, '', ''),
+(4, '', ''),
+(5, '', ''),
+(6, '', '');
 
 -- --------------------------------------------------------
 
@@ -101,10 +72,10 @@ CREATE TABLE IF NOT EXISTS `forum` (
   `N°_Question` int(11) NOT NULL,
   `Theme` varchar(50) NOT NULL,
   `Contenu` text NOT NULL,
-  `Date` datetime NOT NULL,
+  `Date` date NOT NULL,
   `Question_&_Reponse` text NOT NULL,
   PRIMARY KEY (`N°_Question`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -114,23 +85,11 @@ CREATE TABLE IF NOT EXISTS `forum` (
 
 DROP TABLE IF EXISTS `gestionnaire`;
 CREATE TABLE IF NOT EXISTS `gestionnaire` (
-<<<<<<< HEAD
-<<<<<<< HEAD
   `ID_Gestionnaire` int(4) NOT NULL,
-<<<<<<< HEAD
-  `Mot_de_passe` int(20) NOT NULL,
-=======
-=======
-  `ID_Gestionnaire` int(10) NOT NULL AUTO_INCREMENT,
->>>>>>> parent of ba6e8bc... Update appg9b.sql
-=======
-  `ID_Gestionnaire` int(10) NOT NULL AUTO_INCREMENT,
->>>>>>> parent of ba6e8bc... Update appg9b.sql
   `Mot_de_passe` int(255) NOT NULL,
->>>>>>> leopold
   `Nom_auto_ecole` varchar(200) NOT NULL,
   PRIMARY KEY (`ID_Gestionnaire`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -140,7 +99,7 @@ CREATE TABLE IF NOT EXISTS `gestionnaire` (
 
 DROP TABLE IF EXISTS `test`;
 CREATE TABLE IF NOT EXISTS `test` (
-  `N_du_test` int(11) NOT NULL AUTO_INCREMENT,
+  `N°_du_test` int(11) NOT NULL,
   `Date` datetime NOT NULL,
   `Score_total` int(11) NOT NULL,
   `Res_freq_card_avant_test` int(11) NOT NULL,
@@ -152,10 +111,8 @@ CREATE TABLE IF NOT EXISTS `test` (
   `Res_rythme_sonore` int(11) NOT NULL,
   `Res_stimulus_sonore` int(11) NOT NULL,
   `Res_reprod_sonore` int(11) NOT NULL,
-  `ID_Utilisateur` int(10) NOT NULL,
-  `ID_Gestionnaire` int(10) NOT NULL,
-  PRIMARY KEY (`N_du_test`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`N°_du_test`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -165,28 +122,16 @@ CREATE TABLE IF NOT EXISTS `test` (
 
 DROP TABLE IF EXISTS `utilisateur`;
 CREATE TABLE IF NOT EXISTS `utilisateur` (
-<<<<<<< HEAD
-<<<<<<< HEAD
   `IDUtilisateur` int(4) NOT NULL,
-<<<<<<< HEAD
-  `Mot_de_passe` varchar(20) NOT NULL,
-=======
-=======
-  `IDUtilisateur` int(10) NOT NULL AUTO_INCREMENT,
->>>>>>> parent of ba6e8bc... Update appg9b.sql
-=======
-  `IDUtilisateur` int(10) NOT NULL AUTO_INCREMENT,
->>>>>>> parent of ba6e8bc... Update appg9b.sql
   `Mot_de_passe` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
->>>>>>> leopold
   `Nom` varchar(30) NOT NULL,
   `Prenom` varchar(30) NOT NULL,
   `Date_de_naissance` date NOT NULL,
-  `N_de_telephone` int(8) NOT NULL,
+  `N°_de_telephone` int(8) NOT NULL,
   `Adresse` varchar(150) NOT NULL,
   `Adresse_email` varchar(100) NOT NULL,
   PRIMARY KEY (`IDUtilisateur`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
