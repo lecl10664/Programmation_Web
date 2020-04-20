6a08e993ba3c051bbe16ba024c115f6a2dc2470e
janiehnrd <58337469+janiehnrd@users.noreply.github.com>
2020-04-20 13:42:57 +0200

    rechercher utlisateur (gestionnaire)

--- php/gestionnaire_rechercheAdmin.php
+++ php/gestionnaire_rechercheAdmin.php
@@ -15,7 +15,22 @@ chdir($dir2.DIRECTORY_SEPARATOR);
 </head>
 
 <body>
-<p> Utilisateurs</p>
+
+<div id="searchbar">
+    <h1>Rechercher un utilisateur par nom</h1>
+    <form action=""  method="post">
+        <input type="search" name="utilisateur" size="30" placeholder=""/>
+        <input class="button" type="button" value="ok" />
+    </form>
+</div>
+
+<div id="recherche_critères">
+    <h1>Rechercher un utilisateur par critères</h1>
+    <label for="age">Age :</label> <input type="text" name="age" id="age" />
+    <label for="ville">Ville :</label> <input type="text" name="ville" id="ville" />
+    <label for="agence">Agence :</label> <input type="text" name="agence" id="agence" />
+</div>
+
 </body>
 
 <footer>
