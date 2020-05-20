f2c0218dfb5be71bebc474ae5ae2d96f3e9b85c2
ElieAttieh <57668835+ElieAttieh@users.noreply.github.com>
2020-05-20 16:22:16 +0200

    modifs

--- php/footer.php
+++ php/footer.php
@@ -9,7 +9,7 @@
         <meta charset="utf-8">
 
         <title>footer</title>
-        <link rel="stylesheet" href="/css/footer.css">
+        <link rel="stylesheet" href="../css/footer.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
 
--- php/header.php
+++ php/header.php
@@ -11,7 +11,7 @@ if(!isset($_SESSION)){
 <html>
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="/css/header.css">
+    <link rel="stylesheet" href="../css/header.css">
 </head>
 
 
@@ -20,24 +20,24 @@ if(!isset($_SESSION)){
     <p class="cgu_header">
         <a class="linkHeader" href="">Langue : FR  </a>
         - 
-        <a class="linkHeader" href="/php/cgu.php">Conditions générales d'utilisations</a>
+        <a class="linkHeader" href="cgu.php">Conditions générales d'utilisations</a>
         -
-        <a class="linkHeader" href="/php/mentionslegales.php">Mentions légales</a>
+        <a class="linkHeader" href="mentionslegales.php">Mentions légales</a>
         -
-        <a class="linkHeader" href="/php/contact.php">Nous contacter</a>
+        <a class="linkHeader" href="contact.php">Nous contacter</a>
 
     </p>
 
     <div class="deuxieme_partie">
         <div id="logo">
             <a href="../php/pageAccueil.php">
-                <img src="/images/Infinite_measures_logo.png"
+                <img src="../images/Infinite_measures_logo.png"
                      width="150" height="151"
                      alt="logo"/>
             </a>
         </div>
         <div class="title">
-            <img src="/images/Infinite_measures.png" alt="logoTexte"/>
+            <img src="../images/Infinite_measures.png" alt="logoTexte"/>
         </div>
 
         <div class="menu">
