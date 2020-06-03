dad847891ecdd5401781e2d47ed16e739e0ef294
lecl10664 <leopold.clement@isep.fr>
2020-06-03 18:46:55 +0200

    Update appg9b.sql

--- base_de_données/appg9b.sql
+++ base_de_données/appg9b.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3308
--- Généré le :  mer. 03 juin 2020 à 16:29
--- Version du serveur :  10.4.10-MariaDB
+-- Généré le :  mer. 03 juin 2020 à 16:46
+-- Version du serveur :  8.0.18
 -- Version de PHP :  7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -71,7 +71,7 @@ CREATE TABLE IF NOT EXISTS `forum` (
   `Theme` varchar(50) DEFAULT NULL,
   `Contenu` text NOT NULL,
   `Date` date NOT NULL,
-  `Question_&_Reponse` text DEFAULT NULL,
+  `Question_&_Reponse` text,
   `Nom_Utilisateur` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`N°_Question`)
 ) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
@@ -97,15 +97,15 @@ CREATE TABLE IF NOT EXISTS `gestionnaire` (
   `adresse_auto_ecole` varchar(255) NOT NULL,
   `mail_auto_ecole` varchar(255) NOT NULL,
   PRIMARY KEY (`ID_Gestionnaire`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `gestionnaire`
 --
 
 INSERT INTO `gestionnaire` (`ID_Gestionnaire`, `Mot_de_passe`, `Nom_auto_ecole`, `adresse_auto_ecole`, `mail_auto_ecole`) VALUES
-(1, '$2y$10$7HhdkaADnOSHm4WghmOMAuQh2LN4.jGyAvAu9jucndMKOODdka2Mi', 'Auto-ecole Cergy', '100 rue de Cergy', 'cergy@auto-ecole.fr'),
-(2, '$2y$10$zizQqQeKh6AyspPvngdvUOCNE1nVZOCvJGwtfD6qaq2srQXZk8AyO', 'Auto-école Issy', '1 rue d\'Issy', 'issy@auto-ecole.fr');
+(4, '$2y$10$oUm.01iR1nw/3Lks8BakGOJpDiO6G858pfho.NYEzbWCENgWP9POO', 'Auto-école de Cergy', '151 avenue de Cergy', 'cergy@auto-ecole.fr'),
+(3, '$2y$10$MSfW41AeAsKLYCpzB/KrueiCayjm0Lmgv68af/A0OX72qfysPhfW6', 'Auto-école d\'Issy', '1 rue d\'Issy', 'issy@auto-ecole.fr');
 
 -- --------------------------------------------------------
 
@@ -119,7 +119,7 @@ CREATE TABLE IF NOT EXISTS `reponsesforum` (
   `ID_post` int(255) NOT NULL,
   `contenu` text NOT NULL,
   `utilisateur` varchar(100) NOT NULL,
-  `date` datetime NOT NULL DEFAULT current_timestamp(),
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`ID_reponse`)
 ) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
 
